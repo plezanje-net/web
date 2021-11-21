5bdad9016cfb746d93fdc0795131b9df4a4a89c9
Anze Demsar <anze@dspot.si>
2021-11-21 20:39:52 +0100

    preproduction deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://mx3.zakotnik.net:3000/graphql',
-  webUrl: 'https://mx3.zakotnik.net/',
-  storageUrl: 'https://mx3.zakotnik.net/storage',
+  apiUrl: 'https://plezanje.info:3000/graphql',
+  webUrl: 'https://plezanje.info/',
+  storageUrl: 'https://plezanje.info/storage',
 };
