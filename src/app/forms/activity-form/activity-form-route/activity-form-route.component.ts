e53e7d9f0513017425d7f8cd8550be1dd243218f
salamca <tomaz.bevk@gmail.com>
2021-12-31 14:51:58 +0100

    Prevent voting if route is not being ticked.

--- src/app/forms/activity-form/activity-form-route/activity-form-route.component.ts
+++ src/app/forms/activity-form/activity-form-route/activity-form-route.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import {
   ASCENT_TYPES,
@@ -6,13 +13,16 @@ import {
   PUBLISH_OPTIONS,
 } from '../../../common/activity.constants';
 import { Crag } from 'src/generated/graphql';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-activity-form-route',
   templateUrl: './activity-form-route.component.html',
   styleUrls: ['./activity-form-route.component.scss'],
 })
-export class ActivityFormRouteComponent implements OnInit {
+export class ActivityFormRouteComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
+
   @Input() activity = true;
   @Input() route: FormGroup;
   @Input() first: boolean;
@@ -47,5 +57,38 @@ export class ActivityFormRouteComponent implements OnInit {
         }
       }
     );
+
+    const ascentTypeSelected = this.route.get('ascentType').value;
+    this.conditionallyDisableVotedDifficulty(ascentTypeSelected);
+
+    this.route
+      .get('ascentType')
+      .valueChanges.pipe(takeUntil(this.destroy$))
+      .subscribe((at) => this.conditionallyDisableVotedDifficulty(at));
+  }
+
+  /**
+   *
+   * @param ascentTypeSelected
+   *
+   * Make voting on difficulty possible only if a user is ticking a route
+   */
+  conditionallyDisableVotedDifficulty(ascentTypeSelected: string) {
+    const isTick = ASCENT_TYPES.some(
+      (at) => at.value === ascentTypeSelected && at.tick
+    );
+
+    // Only if route is being newly ticked or repeated (which also is a tick) can one cast a vote on difficulty (i.e. new vote or modifying existing one)
+    if (isTick) {
+      this.route.get('votedDifficulty').enable();
+    } else {
+      this.route.get('votedDifficulty').setValue(null);
+      this.route.get('votedDifficulty').disable();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
