8c24f8d192f7d1206b9e5858e20cf1dee31cb16e
salamca <tomaz.bevk@gmail.com>
2022-01-17 18:55:05 +0100

    Fixed layout on crags list view to accomodate enough space for orientation strings.

--- src/app/shared/pipes/orientation.pipe.ts
+++ src/app/shared/pipes/orientation.pipe.ts
@@ -4,12 +4,12 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'orientation',
 })
 export class OrientationPipe implements PipeTransform {
-  transform(value: string): string {
+  transform(value: string, display = 'full'): string {
     const sides = {
-      N: 'sever',
-      S: 'jug',
-      E: 'vzhod',
-      W: 'zahod',
+      N: { full: 'sever', short: 'S' },
+      S: { full: 'jug', short: 'J' },
+      E: { full: 'vzhod', short: 'V' },
+      W: { full: 'zahod', short: 'Z' },
     };
 
     if (value == null) {
@@ -17,15 +17,19 @@ export class OrientationPipe implements PipeTransform {
     }
 
     if (value.length == 1) {
-      return sides[value] || '';
+      return sides[value][display] || '';
     }
 
     if (
       value.length == 2 &&
-      sides[value[0]] != null &&
-      sides[value[1]] != null
+      sides[value[0]][display] != null &&
+      sides[value[1]][display] != null
     ) {
-      return sides[value[0]] + 'o' + sides[value[1]];
+      return (
+        sides[value[0]][display] +
+        (display === 'full' ? 'o' : '') +
+        sides[value[1]][display]
+      );
     }
 
     return '';
