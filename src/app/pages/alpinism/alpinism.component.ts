d79a414e7be0a3ec9665434e7a88c2d2db342931
salamca <tomaz.bevk@gmail.com>
2022-02-18 14:59:15 +0100

    Set breadcrumb on alpinism page.

--- src/app/pages/alpinism/alpinism.component.ts
+++ src/app/pages/alpinism/alpinism.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { LayoutService } from 'src/app/services/layout.service';
 
 @Component({
   selector: 'app-alpinism',
   templateUrl: './alpinism.component.html',
-  styleUrls: ['./alpinism.component.scss']
+  styleUrls: ['./alpinism.component.scss'],
 })
 export class AlpinismComponent implements OnInit {
-
-  constructor() { }
+  constructor(private layoutService: LayoutService) {}
 
   ngOnInit(): void {
+    this.layoutService.$breadcrumbs.next([
+      {
+        name: 'Alpinizem',
+      },
+    ]);
   }
-
 }
