40026ec4a8b537f8dddbbceb4215f37a93c0eff7
Nejc Stebe <nejc@cleanshelf.com>
2022-03-26 17:04:01 +0100

    footer improvements

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { LayoutService } from 'src/app/services/layout.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  constructor(private layoutService: LayoutService) {}
 
   ngOnInit(): void {
+    this.layoutService.$breadcrumbs.next([
+      {
+        name: 'O plezanje.net',
+      },
+    ]);
   }
-
 }
