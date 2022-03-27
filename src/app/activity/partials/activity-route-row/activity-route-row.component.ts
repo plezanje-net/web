d4642108f9219f354618b12ae8418e7f89ae5f7e
Anze Demsar <demshy@gmail.com>
2022-03-27 21:16:21 +0200

    change activiy route visibility from list

--- src/app/activity/partials/activity-route-row/activity-route-row.component.ts
+++ src/app/activity/partials/activity-route-row/activity-route-row.component.ts
@@ -1,6 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Subject } from 'rxjs';
-import { ActivityRoute } from 'src/generated/graphql';
+import { PUBLISH_OPTIONS } from 'src/app/common/activity.constants';
+import {
+  ActivityRoute,
+  ActivityRouteChangePublishGQL,
+} from 'src/generated/graphql';
 import { RowAction } from '../../pages/activity-log/activity-log.component';
 
 @Component({
@@ -12,8 +17,32 @@ export class ActivityRouteRowComponent implements OnInit {
   @Input() route: ActivityRoute;
   @Input() rowAction: Subject<RowAction>;
   @Input() displayType: 'activity' | 'routes' = 'routes';
+  publishOptions = PUBLISH_OPTIONS;
 
-  constructor() {}
+  constructor(
+    private activityRouteChangePublishGQL: ActivityRouteChangePublishGQL,
+    private snackbar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {}
+
+  changePublish(value: string) {
+    this.activityRouteChangePublishGQL
+      .mutate({
+        input: { id: this.route.id, publish: value },
+      })
+      .subscribe({
+        next: () => {
+          this.snackbar.open('Vidnost vzpona je bila spremenjena', null, {
+            duration: 2000,
+          });
+        },
+        error: () => {
+          this.snackbar.open('Vidnosti ni bilo mogoče spremeniti', null, {
+            panelClass: 'error',
+            duration: 3000,
+          });
+        },
+      });
+  }
 }
