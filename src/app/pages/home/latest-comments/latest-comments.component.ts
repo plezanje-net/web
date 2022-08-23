dc3fe55dc904e8fb48a901d10ffedcb54956b1ea
Nejc Stebe <nejc@cleanshelf.com>
2022-08-23 22:24:48 +0200

    Render queried comments

--- src/app/pages/home/latest-comments/latest-comments.component.ts
+++ src/app/pages/home/latest-comments/latest-comments.component.ts
@@ -8,6 +8,22 @@ import { LatestCommentsGQL } from 'src/generated/graphql';
 })
 export class LatestCommentsComponent implements OnInit {
   loading = true;
+  comments: {
+    __typename?: 'Comment';
+    id: string;
+    type: string;
+    content?: string;
+    created: any;
+    updated: any;
+    user?: { __typename?: 'User'; id: string; fullName: string };
+    crag?: {
+      __typename?: 'Crag';
+      name: string;
+      slug: string;
+      country: { __typename?: 'Country'; slug: string; name: string };
+    };
+    route?: { __typename?: 'Route'; name: string };
+  }[];
 
   constructor(private latestCommentsGQL: LatestCommentsGQL) {}
 
@@ -18,8 +34,9 @@ export class LatestCommentsComponent implements OnInit {
 
     latestComments$.subscribe({
       next: (result) => {
+        this.comments = result.data.latestComments;
         console.log(result.data.latestComments);
-        // TODO shrani komentarje in jih izriši
+        this.loading = false;
       },
       error: () => {
         console.log('TODO');
