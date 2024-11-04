d3414058c5a6a33e3d808dcb8131d1ee6c858293
Anze Demsar <demshy@gmail.com>
2024-11-04 12:21:06 +0100

    correct prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://plezanje.net/graphql',
-  uploadUrl: 'https://plezanje.net/upload',
-  webUrl: 'https://plezanje.net/',
-  storageUrl: 'https://plezanje.net/storage',
+  apiUrl: 'https://plezanjenet.ademsar.eu/graphql',
+  uploadUrl: 'https://plezanjenet.ademsar.eu/upload',
+  webUrl: 'https://plezanjenet.ademsar.eu/',
+  storageUrl: 'https://plezanjenet.ademsar.eu/storage',
   storageKeyPrefix: 'plezanje-2',
   sentryDsn:
     'https://a6bd493f5c044c419cadb227cfe067f6@o1179288.ingest.sentry.io/6291581',
-  sentryTracingUrl: 'https://plezanje.net/graphql',
+  sentryTracingUrl: 'https://plezanjenet.ademsar.eu/graphql',
 };
