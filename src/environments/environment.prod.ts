65b4a25d938e7755775cee5e8b3d158ba11fd24a
Anze Demsar <demshy@gmail.com>
2025-12-12 11:01:34 +0100

    add coolify environment vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://plezanje.net/graphql',
-  uploadUrl: 'https://plezanje.net/upload',
-  webUrl: 'https://plezanje.net/',
-  storageUrl: 'https://plezanje.net/storage',
+  apiUrl: 'https://api.plezanje.eu/graphql',
+  uploadUrl: 'https://api.plezanje.eu/upload',
+  webUrl: 'https://plezanje.eu/',
+  storageUrl: 'https://plezanje.eu/storage',
   storageKeyPrefix: 'plezanje-2',
   sentryDsn:
     'https://a6bd493f5c044c419cadb227cfe067f6@o1179288.ingest.sentry.io/6291581',
-  sentryTracingUrl: 'https://plezanje.net/graphql',
+  sentryTracingUrl: 'https://api.plezanje.eu/graphql',
 };
